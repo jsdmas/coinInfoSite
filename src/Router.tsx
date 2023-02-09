a2ceec2afb727c8613158efd0126f0c4285ac03d
jsdmas <sdmas5730@gmail.com>
2023-02-09 18:15:22 +0900

    edit

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ import Price from "./routes/Price";
 
 const Router = () => {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter >
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/:coinId" element={<Coin />}>
